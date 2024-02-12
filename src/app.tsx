39daf1b10920e2ee7c2ff00b66294dfb5b8ce4a7
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-12 20:09:16 -0300

    first lesson finished :tada:

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nlw-export.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
@@ -15,28 +17,17 @@ export function App() {
       <div className='h-px bg-slate-700' />
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
-          <span className='text-sm font-medium text-slate-200'>
-            Adcionar uma nova nota
-          </span>
-          <p className='text-sm leading-6 text-slate-400'>
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
+        <NewNoteCard />
 
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 relative'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 2 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400'>
-            O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where...
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
 
-            Para configurar o plugin, é preciso instalar como abaixo:
-          </p>
-
-          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </div>
-
+       
         <div className='rounded-md bg-slate-800 p-5 space-y-3 relative'>
           <span className='text-sm font-medium text-slate-300'>
             há 4 dias
