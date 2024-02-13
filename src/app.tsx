b8a3b35b67c83e363d7885fba04ce9ab1557c9e2
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-13 17:33:26 -0300

    Format app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,71 @@
-import logo from './assets/logo-nlw-export.svg'
-import { NewNoteCard } from './components/new-note-card'
-import { NoteCard } from './components/note-card'
+import { ChangeEvent, useState } from "react";
+import logo from "./assets/logo-nlw-export.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
 
 export function App() {
+  const [search, setSearch] = useState("");
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorate = localStorage.getItem("notes");
+    if (notesOnStorate) {
+      return JSON.parse(notesOnStorate);
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const newNotes = [newNote, ...notes];
+    setNotes(newNotes);
+
+    localStorage.setItem("notes", JSON.stringify(newNotes));
+  }
+
+  function handleSearc(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  const notesFilter =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
+
   return (
-    <div className='max-w-6xl mx-auto my-10 space-y-6'>
-      <img src={logo} alt='NLW Expert' />
-      <form className='w-full'>
-        <input 
-          type="text" 
-          placeholder='Busque em suas notas...' 
-          className='w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500'
+    <div className="max-w-6xl mx-auto my-10 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500"
+          onChange={handleSearc}
+          value={search}
         />
       </form>
 
-      <div className='h-px bg-slate-700' />
+      <div className="h-px bg-slate-700" />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard />
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        <NoteCard 
-          note={{
-            date: new Date(),
-            content: `O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... \nPara configurar o plugin, é preciso instalar como abaixo:`
-          }}
-        />
-        <NoteCard 
-          note={{
-            date: new Date(),
-            content:  `No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade. \nNa aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio.`
-          }}
-        />
-        
-
+        {notesFilter.map((note) => (
+          <NoteCard key={note.id} note={note} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
-
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,9 +3,12 @@ import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
 import { toast } from 'sonner'
 
-
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string)=>void
+}
+export function NewNoteCard({onNoteCreated}:NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [isRecording, setIsRecording] = useState(false)
   const [content, setContent] = useState('')
 
   function handleStartEditor() {
@@ -22,9 +25,18 @@ export function NewNoteCard() {
   function handleSaveNote(event: FormEvent) {
     event.preventDefault()
 
-    console.log(content)
-
+    onNoteCreated(content)
+    setContent('')
+    setShouldShowOnboarding(true)
     toast.success('Nota criada com sucesso!')
+  }
+
+  function handleStartRecording(){
+    setIsRecording(true)
+  }
+
+  function handleStopRecording(){
+    setIsRecording(false)
   }
 
 
@@ -55,23 +67,37 @@ export function NewNoteCard() {
               {
                 shouldShowOnboarding ? (
                   <p className="text-sm leading-6 text-slate-400">
-                    Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
+                    Comece <button type='button' onClick={handleStartRecording} className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button type='button' onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
                   </p>
                 ) : (
                   <textarea 
                     autoFocus
                     className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
                     onChange={handleContentChange}
+                    value={content}
                   />
                 )
               }
             </div>
-            <button 
-              type='submit'
-              className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
-            >
-              Salvar nota
-            </button>
+
+            {isRecording ? (
+              <button 
+                type='button'
+                onClick={handleStopRecording}
+                className='w-full bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100'
+              >
+                Gravando! (clique para interromper)
+              </button>
+            ) : (
+              <button 
+                type='submit'
+                className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+              >
+                Salvar nota
+              </button>
+            )}
+
+
           </form>
         </Dialog.Content>
       </Dialog.Portal>
