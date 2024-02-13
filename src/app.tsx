73e1009cc9b747997eea1b0951bd64a3ade10380
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-13 19:07:33 -0300

    create function delete note and finshed application :tada:

--- src/app.tsx
+++ src/app.tsx
@@ -33,6 +33,16 @@ export function App() {
     localStorage.setItem("notes", JSON.stringify(newNotes));
   }
 
+  function onNoteDeleted(id: string) {
+    const newNotes = notes.filter(note=> {
+      return note.id !== id
+    })
+
+    setNotes(newNotes)
+    localStorage.setItem("notes", JSON.stringify(newNotes));
+
+  }
+
   function handleSearc(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
   }
@@ -45,7 +55,7 @@ export function App() {
       : notes;
 
   return (
-    <div className="max-w-6xl mx-auto my-10 space-y-6">
+    <div className="max-w-6xl mx-auto my-10 space-y-6 px-5 xl:px-0">
       <img src={logo} alt="NLW Expert" />
       <form className="w-full">
         <input
@@ -59,11 +69,11 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {notesFilter.map((note) => (
-          <NoteCard key={note.id} note={note} />
+          <NoteCard key={note.id} note={note} handleDeleteNote={onNoteDeleted} />
         ))}
       </div>
     </div>
