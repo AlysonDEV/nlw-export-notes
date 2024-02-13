ffe9089f241b66a1166b9e104d11062ffce8fb74
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-12 21:29:09 -0300

    finished the designe before functions

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import logo from './assets/logo-nlw-export.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
 export function App() {
   return (
     <div className='max-w-6xl mx-auto my-10 space-y-6'>
@@ -19,28 +20,20 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard 
+          note={{
+            date: new Date(),
+            content: `O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... \nPara configurar o plugin, é preciso instalar como abaixo:`
+          }}
+        />
+        <NoteCard 
+          note={{
+            date: new Date(),
+            content:  `No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade. \nNa aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio.`
+          }}
+        />
+        
 
-       
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 relative'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 4 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400'>
-            No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade.
-
-            Na aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio.
-          </p>
-
-          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-
-        </div>
       </div>
     </div>
   )
